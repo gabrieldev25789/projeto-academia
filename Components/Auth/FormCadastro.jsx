56b7cf1373aeedc9e654c68050ca4448377aa67b
Gabriel <gabrielsb1090@gmail.com>
2026-09-21 03:52:01 -0300

    feat: Estruturando form

--- Components/Auth/FormCadastro.jsx
+++ Components/Auth/FormCadastro.jsx
@@ -1,8 +1,59 @@
 import "./FormCadastro.css"
+
 function FormCadastro() {
   return (
-    <div>
-        <h2>AAAAAAAAASDLJADSJSDA</h2>
+    <div className="cadastro-container">
+      <form className="form-cadastro">
+        <h2>Criar conta</h2>
+
+        <div className="campo">
+          <label htmlFor="nome">Nome</label>
+          <input 
+            type="text" 
+            id="nome" 
+            name="nome" 
+            placeholder="Digite seu nome"
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="email">E-mail</label>
+          <input 
+            type="email" 
+            id="email" 
+            name="email" 
+            placeholder="Digite seu e-mail"
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="senha">Senha</label>
+          <input 
+            type="password" 
+            id="senha" 
+            name="senha" 
+            placeholder="Crie uma senha"
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="confirmarSenha">Confirmar senha</label>
+          <input 
+            type="password" 
+            id="confirmarSenha" 
+            name="confirmarSenha" 
+            placeholder="Repita a senha"
+          />
+        </div>
+
+        <button type="submit" className="botao-cadastrar">
+          Cadastrar
+        </button>
+
+        <p className="link-login">
+          Já tem uma conta? <a href="#">Entrar</a>
+        </p>
+      </form>
     </div>
   )
 }
