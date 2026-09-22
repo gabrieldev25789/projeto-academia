8ab8ff927863a3f0884ef22564d31383abe9ff85
Gabriel <gabrielsb1090@gmail.com>
2026-09-22 02:31:01 -0300

    feat: Estruturando Onboarding

---
+++ Components/Onboarding/Onboarding.jsx
@@ -0,0 +1,43 @@
+import IndicadorProgresso from "./IndicadorProgresso"
+import EtapaObjetivo from "./Etapaobjetivo"
+import EtapaMedidas from "./Etapamedidas"
+import EtapaExperiencia from "./Etapaexperiencia"
+
+import { useState } from "react"
+
+function Onboarding({ usuario, setUsuarioLogado }) {
+  const [etapa, setEtapa] = useState(1)
+  const [dadosOnboarding, setDadosOnboarding] = useState({
+    objetivo: "",
+    peso: "",
+    altura: "",
+    idade: "",
+    experiencia: ""
+  })
+
+  function finalizarOnboarding() {
+    const usuarioAtualizado = {
+      ...usuario,
+      ...dadosOnboarding,
+      onboardingCompleto: true
+    }
+
+    localStorage.setItem("user", JSON.stringify(usuarioAtualizado))
+    setUsuarioLogado(usuarioAtualizado)
+  }
+
+  return (
+    <div className="onboarding-container">
+      <IndicadorProgresso etapaAtual={etapa} totalEtapas={3} />
+
+      {etapa === 1 && <EtapaObjetivo dados={dadosOnboarding} setDados={setDadosOnboarding} onProximo={() => setEtapa(2)} />}
+
+      {etapa === 2 && <EtapaMedidas dados={dadosOnboarding} setDados={setDadosOnboarding} onProximo={() => setEtapa(3)} onVoltar={() => setEtapa(1)} />}
+
+      {etapa === 3 && <EtapaExperiencia dados={dadosOnboarding} setDados={setDadosOnboarding} onFinalizar={finalizarOnboarding} onVoltar={() => setEtapa(2)} />}
+        
+    </div>
+  )
+}
+
+export default Onboarding
