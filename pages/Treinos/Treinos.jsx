1cfec40270e3381caa08bb3134d792f032ca3532
Gabriel <gabrielsb1090@gmail.com>
2026-09-24 01:55:31 -0300

    feat: Criando componente de treino

---
+++ pages/Treinos/Treinos.jsx
@@ -0,0 +1,11 @@
+
+function Treinos() {
+  return (
+    <div className="treinos-container">
+      <h1>Treinos</h1>
+      <p>Nenhum treino cadastrado ainda</p>
+    </div>
+  )
+}
+
+export default Treinos
