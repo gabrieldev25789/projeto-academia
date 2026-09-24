dd2e6ff6cb6af7e9495735ecec4c221ea27f52dd
Gabriel <gabrielsb1090@gmail.com>
2026-09-24 02:55:16 -0300

    feat: Estruturando componente de treinos

--- pages/Treinos/Treinos.jsx
+++ pages/Treinos/Treinos.jsx
@@ -1,9 +1,25 @@
+import { useLocation } from "react-router-dom"
+import { useState } from "react"
+import FormTreino from "./FormTreino"
 
 function Treinos() {
+    const location = useLocation()
+    const [criarTreino, setCriarTreino] = useState(location.state?.abrirFormulario || false)
+
   return (
     <div className="treinos-container">
-      <h1>Treinos</h1>
-      <p>Nenhum treino cadastrado ainda</p>
+      {criarTreino ? (
+        <div>
+          <h1>Novo treino</h1>
+          <FormTreino />
+        </div>
+      ) : (
+        <div>
+          <h1>Treinos</h1>
+          <p>Nenhum treino cadastrado ainda</p>
+          <button onClick={() => setCriarTreino(true)}>Criar treino</button>
+        </div>
+      )}
     </div>
   )
 }
