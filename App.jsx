5e7cc84609b3d9a14fbb2c6a748e265c18b864de
Gabriel <gabrielsb1090@gmail.com>
2026-09-24 02:53:37 -0300

    feat: Adicionando emailSessao para pagina não mudar ao atualizar

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Routes, Route, Navigate } from "react-router-dom"
 import "../global.css"
 import FormCadastro from './Components/Auth/FormCadastro'
@@ -6,11 +6,24 @@ import Header from "./Components/Header/Header"
 import Home from "./Components/Home/Home"
 import Onboarding from "./Components/Onboarding/Onboarding"
 import { ImcProvider } from "./Components/ImcContext"
-import Treinos from "./pages/Treinos/Treinos"
+import Treinos from "./pages/Treinos/Treinos.jsx"
 
 function App() {
 
   const [usuarioLogado, setUsuarioLogado] = useState(null)
+   
+  useEffect(() => {
+    const emailSessao = localStorage.getItem("sessaoAtual")
+ 
+    if (emailSessao) {
+      const usuarios = JSON.parse(localStorage.getItem("usuarios")) || []
+      const usuarioEncontrado = usuarios.find(u => u.email === emailSessao)
+ 
+      if (usuarioEncontrado) {
+        setUsuarioLogado(usuarioEncontrado)
+      }
+    }
+  }, [])
 
   return (
     <ImcProvider>
